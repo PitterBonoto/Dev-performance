358cc58b052077fb1fdf28766dc703d62fb8404f
fabio.toledo12 <fvtoledojf@hotmail.com>
2024-03-25 19:40:40 -0300

    Subir Tela Saude Pronta

---
+++ src/Pages/Saude/styles.js
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+import BackgroundFundo from "../../assets/background-2.png";
+
+export const ContainerPrincipal = styled.div`
+  background: url("${BackgroundFundo}");
+  background-size: cover;
+  height: 100vh;
+  padding: 15px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+ `;
+
+export const Logo = styled.img`
+width: 200px;
+`;
+
+export const ContainerItensPrincipal = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  //background: #000;
+  z-index: 1;
+` 
+
+export const ContainerItens = styled.div`
+  background: linear-gradient(
+    157deg,
+    rgba(255, 255, 255, 0) 0.2%,
+    rgba(255, 255, 255, 0) 0.5%,
+    rgba(255, 255, 255, 0.2) 100%
+  );
+  backdrop-filter: blur(10px);
+  border-radius: 15px;
+  width: 280px;
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+`;
+
+export const TitleCard = styled.p`
+  color: #2c2b2b;
+  font-weight: 600;
+  font-size: 17px;
+  text-align: center;
+`
+
+export const ListaCard = styled.ol`
+  color: #000000;
+  font-weight: 400;
+  font-size: 14px;
+  text-align: center;
+  padding: 15px;
+  li{
+    text-align: left;
+  }
+`
+
+export const Button = styled.button`
+  border-radius: 15px;
+  padding: 15px 0;
+  border: none;
+  background-color: #ea4d2a;
+  color: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  font-size: 14px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  &:active {
+    opacity: 0.4;
+  }
+`
+
+export const ContainerCitation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: -17% ;
+  //background: #ea4d2a;
+`
+
+export const Citation = styled.p`
+  color: #2c2b2b;
+  font-weight: 600;
+  font-size: 18px;
+  width: 55%;
+`;
+
+export const ImgSaude = styled.img`
+  weight: 250px;
+  height: 250px;
+  right: 0;
+`
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import GlobalStyle from "./globalStyle";
 import Login from "./Pages/Login";
-import Home from "./Pages/Home"
+import Home from "./Pages/Home";
+import Saude from "./Pages/Saude"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Login />
+    <Saude />
     <GlobalStyle />
   </>
 );
