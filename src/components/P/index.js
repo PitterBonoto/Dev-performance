45818b1a50b2918f43d18eadcfb2c9da98f0fb11
PitterBonoto <pitterbonoto@gmail.com>
2024-04-05 19:26:00 -0300

    alteração

--- src/components/P/index.js
+++ src/components/P/index.js
@@ -3,10 +3,6 @@ import React from "react";
 import { P } from "../../Pages/Health/styles";
 
 export function Paragraph({ children, isTaskCompleted }) {
-<<<<<<< HEAD
- // console.log(isTaskCompleted);
-=======
   //console.log(isTaskCompleted)
->>>>>>> 681b90a60437cb91ac1fdbe6f819e65de9bf986a
   return <P isTaskCompleted={isTaskCompleted}>{children}</P>;
 }
